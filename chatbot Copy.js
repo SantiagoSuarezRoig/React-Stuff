4c085883b5f84d90ddbaf5a382be79a102ac9268
SuarezRoig <santiagosuarez2604@gmail.com>
2025-12-13 14:54:10 +0100

    adding some loading to the bot response and styling

--- chatbot Copy.js
+++ chatbot Copy.js
@@ -1,42 +1,88 @@
 
 
 
+function Reset({setClicks}){
 
-function PriceProduct({name,price,discountPrice,imagSrc}){
+    function resetCount(){
+        setClicks(0)
+    }
     return(
-        <div>
-            <img src={imagSrc} width="150"></img>
-            <p>{name}</p>
-            {discountPrice?
-            (<>  
-                <del><p>Price: ${price}</p></del>
-                <p>Discount Price: ${discountPrice}</p>
-            </>) 
-            : (<p>Price: ${price}</p>)}
+        <button
+        onClick={resetCount}>Reset</button>
+    )
+}
+ 
 
-            <button>Add to Cart</button>    
+function ButtonRender({clicks,setClicks}){
+    function onClicks(){
+        setClicks(clicks +1)
+    }
+
+    return(clicks == 1 ? 
+        <button
+        onClick= {onClicks}>
+            Clicked {clicks} time
+        </button> :
+        <button
+        onClick= {onClicks}>
+            Clicked {clicks} times
+        </button>
+    );
+}
+
+
+
+function InputShit(){
+    const [inputValue,setInputValue] = React.useState('')
+    const example = 'Santiago'
+
+    function saveInput(event){
+        setInputValue(event.target.value)
+    }
+
+    function putExample(){
+        setInputValue(example)
+    }
+
+    function reset(){
+        setInputValue('')
+    }
+
+    return (
+        <div>
+            <input 
+            onChange={saveInput}
+            type="text"
+            placeholder="Di algo puto"
+            value= {inputValue}
+            />
+            <p>Hola {inputValue}</p>
+            
+            <button
+            onClick={reset}>Reset</button>
+            
+            <button
+            onClick={putExample}>Example</button>
+        
         </div>
     )
 }
 
 
+
+
 function App(){
+    const [clicks,setClicks] = React.useState(0)
     return (
     <>
-        <PriceProduct 
-            name="Cotton socks"
-            discountPrice="5.45"
-            price="10.90"
-            imagSrc='images/cotton-socks.png' />
-        <PriceProduct 
-            name="Tennis Ball"
-            price="6.00"
-            imagSrc='images/tennis-balls.png' />
-        <PriceProduct 
-            name="Plain T-shirt"
-            discountPrice="5.45"
-            price="7.99"
-            imagSrc='images/plain-t-shirt.png' />
+        <ButtonRender 
+        clicks={clicks}
+        setClicks={setClicks}/>
+
+        <Reset
+        setClicks={setClicks}/>
+
+        <InputShit />
     </>
     );
 }
